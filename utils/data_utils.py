bd98f01610fc8063b639202433ecdb70257166b5
gebilxs <1799676669@qq.com>
2024-02-02 21:17:09 +0800

    Add random seed initialization and read public data

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,7 +1,18 @@
 import numpy as np
 import os
 import torch
-
+import random
+import numpy as np
+import os
+SEED = 2021
+random.seed(SEED)
+np.random.seed(SEED)
+os.environ['PYTHONHASHSEED'] = str(SEED)
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+torch.cuda.manual_seed_all(SEED)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = True
 
 def read_data(dataset, idx, is_train=True):
     if is_train:
@@ -23,7 +34,27 @@ def read_data(dataset, idx, is_train=True):
 
         return test_data
 
+# def read_p_data(public_dataset,is_train=True):
+#     if is_train:
+#         public_data_dir = os.path.join('dataset',public_dataset+'_public')
+#         public_file = public_data_dir + '/0.npz'
+#         with open(public_file,'rb') as f:
+#             public_data = np.load(f,allow_pickle=True)['x'].tolist()
+#         return public_data
+    
+def read_public_data(public_dataset,is_train=True):
+    if is_train:
+        public_data_dir = os.path.join('dataset',public_dataset+'_public')
+        public_file = public_data_dir + '/0.npz'
+        with np.load(public_file,allow_pickle=True) as public_dataset:
+            X_public = torch.Tensor(public_dataset['x']).type(torch.float32)
+            X_public = X_public.unsqueeze(1)  # 添加一个通道维度
+            # print(X_public.shape)
+            y_public = torch.Tensor(public_dataset['y']).type(torch.long)
 
+            public_data = [(x,y) for x,y in zip(X_public,y_public)]
+
+    return public_data    
 def read_client_data(dataset, idx, is_train=True):
     if dataset[:2] == "ag" or dataset[:2] == "SS":
         return read_client_data_text(dataset, idx, is_train)
